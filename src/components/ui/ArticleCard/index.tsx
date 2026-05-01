420b75050c118acae93bde74be5aef4fdf87b38d
Rafael Menezes <menezes.inbox@gmail.com>
2026-05-01 18:35:30 -0300

    feat: wip

--- src/components/ui/ArticleCard/index.tsx
+++ src/components/ui/ArticleCard/index.tsx
@@ -15,8 +15,8 @@ export const ArticleCard = async ({ article }: { article: Article }) => {
             className={cn(
                 "block rounded-2xl",
                 "transition-[background,box-shadow,transform] duration-200 ease-out",
-                "hover:-translate-y-0.5 hover:bg-white hover:shadow-[0_4px_16px_-2px_rgba(0,0,0,0.06),0_1px_4px_-1px_rgba(0,0,0,0.04)]",
-                "active:shadow-[0_4px_12px_-2px_rgba(0,0,0,0.2),0_1px_4px_-1px_rgba(0,0,0,0.1)]",
+                "hover:-translate-y-0.5 hover:bg-white hover:shadow-[0_2px_16px_-2px_rgba(0,0,0,0.3),0_1px_2px_-1px_rgba(0,0,0,0.1)]",
+                "active:shadow-[0_1px_12px_-2px_rgba(0,0,0,0.6),0_1px_1px_-1px_rgba(0,0,0,0.1)]",
                 "focus-visible:ring-primary-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
             )}
         >
