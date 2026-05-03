05771e5f62e80672c550092e175b8303399dd041
Rafael Menezes <menezes.inbox@gmail.com>
2026-05-03 16:00:41 -0300

    fix: seo

--- src/app/sitemap.ts
+++
@@ -1,6 +0,0 @@
-export default function sitemap() {
-    return [
-        { url: "https://seudominio.com/pt", lastModified: new Date() }, // @TODO
-        { url: "https://seudominio.com/en", lastModified: new Date() }, // @TODO
-    ];
-}
